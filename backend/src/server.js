8a21543669e29290bb58c957d59fa51d305a9097
Mihir Parmar <mihirparmar766@gmail.com>
2026-05-01 11:26:43 +0530

    deployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -98,10 +98,20 @@ connectDB();
 const app = express();
 
 // Enable CORS
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-     // frontend URL
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
